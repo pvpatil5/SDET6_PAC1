20e9a6f13d01f767b6810a78e6e574e7f4e4425d
pvpatil5 <pvpatil123@yahoo.com>
2022-05-05 16:34:18 +0530

    DEMOM 3

--- src/com/pac/DEMO3.java
+++ src/com/pac/DEMO3.java
@@ -1,12 +1,10 @@
 package com.pac;
 
-public class DEMO2 {
+public class DEMO3 {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
-		System.out.println("hi");
 
-		System.out.println("HELLO");
 		System.out.println("HELLO");
 	}
 
