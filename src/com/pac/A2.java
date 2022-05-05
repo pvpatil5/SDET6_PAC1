d99d2e960dfae6f2548c52130624f715b12767de
pvpatil5 <pvpatil123@yahoo.com>
2022-05-05 16:52:41 +0530

    QW

---
+++ src/com/pac/A2.java
@@ -0,0 +1,10 @@
+package com.pac;
+
+public class A2 {
+
+	public static void main(String[] args) {
+		System.out.println("A2");
+
+	}
+
+}
---
+++ src/com/pac/A3.java
@@ -0,0 +1,10 @@
+package com.pac;
+
+public class A3 {
+
+	public static void main(String[] args) {
+		System.out.println("A3");
+
+	}
+
+}
